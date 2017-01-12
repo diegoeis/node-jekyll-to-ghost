c255a6cb6b9ad70a080f6f3184612e930f5d2b29
saltfactory <saltfactory@gmail.com>
2017-01-12 16:09:24 +0900

    first import

---
+++ index.js
@@ -0,0 +1,142 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const grayMatter = require('gray-matter');
+const uuid = require('node-uuid');
+const markdown = require('markdown').markdown;
+const extend = require('util')._extend;
+
+
+let tagList = [];
+let matterList = [];
+
+let ghostData = {
+  meta: {
+    exported_on: Date.now(),
+    version: "000"
+  },
+  data: {
+    posts: [],
+    tags: [],
+    posts_tags: []
+  }
+};
+
+
+
+function getMatter(filePath) {
+  let fileContent = String(fs.readFileSync(filePath));
+  return grayMatter(fileContent);
+}
+
+
+function createSlug(filename) {
+  return filename.substring(11, filename.lastIndexOf('.'));
+}
+
+function createDate(filename) {
+  return Date.parse(filename.substring(0, 10));
+}
+
+function addUniqueTags(tags, index) {
+  let postIndex = index+1;
+
+  let addUniqueTag = (tag) => {
+    if (tagList.indexOf(tag) === -1){
+      tagList.push(tag);
+    }
+
+    createPostsTags(postIndex, tagList.indexOf(tag)+1);
+
+  };
+
+  if (typeof tags === 'string') {
+    addUniqueTag(tags);
+  } else if(typeof tags === 'object') {
+    tags.map(tag => addUniqueTag(tag));
+  }
+
+}
+
+function createGhostPost(index, matter, filename) {
+  let importUserId = 1;
+  let post = {
+    featured: 0,
+    page: 0,
+    status: 'published',
+    language: 'ko_KR',
+    meta_description: null,
+    author_id: importUserId,
+    created_by: importUserId,
+    updated_by: importUserId,
+    published_by: importUserId,
+  };
+
+  post = extend(post, {
+    id: index + 1,
+    uuid: uuid.v4(),
+    title: matter.data.title,
+    slug: createSlug(filename),
+    markdown: matter.content,
+    html: markdown.toHTML(matter.content),
+    image: matter.data.images ? matter.data.images.title : null,
+    meta_title: matter.data.title,
+    created_at: createDate(filename),
+    updated_at: createDate(filename),
+    publised_at: createDate(filename)
+  });
+
+  return post;
+}
+
+function createGhostTag(index, tag) {
+  return {
+    id: index + 1,
+    name: tag,
+    slug: tag,
+    description: ""
+  };
+}
+
+function createPostsTags(postId, tagId){
+  ghostData.data.posts_tags.push({post_id:postId, tag_id:tagId});
+}
+
+
+function convert(src, dest){
+  fs.readdir(src, (err, filenames) => {
+    filenames.forEach((filename, index) => {
+      let filePath = path.join(src, filename);
+      let matter = getMatter(filePath);
+      matterList.push(matter);
+
+      let ghostPost = createGhostPost(index, matter, filename);
+      ghostData.data.posts.push(ghostPost);
+      addUniqueTags(matter.data.tags, index);
+    });
+
+    tagList.forEach((tag, i) => {
+      let ghostTag = createGhostTag(i, tag);
+      ghostData.data.tags.push(ghostTag);
+    });
+
+    createPostsTags();
+    fs.writeFile(dest, JSON.stringify(ghostData), err => err ? console.error(err) : process.exit(0));
+  });
+}
+
+if ( process.argv.length < 4) {
+  console.error('You need to specify a path to Jekyll posts.');
+} else {
+  let src = process.argv[2];
+  let dest = process.argv[3];
+
+  convert(src, dest);
+}
+
+
+
+
+
+
